375e42577e2cfb962afb91e6d5060b4268a01b9b
Кротов Георгий <zvuk24@gmail.com>
2022-10-15 22:00:09 +0700

    23

---
+++ File_handler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace Dict_CS
+{
+    class File_handler // класс для загрузки и выгрузки словарей в файл
+    {
+        public JsonSerializer jss;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace Dict_CS
 {
@@ -11,6 +13,26 @@ namespace Dict_CS
         static void Main(string[] args)
         {
             Dictionary<string, string> D_rus_eng = new Dictionary<string, string>(); //русско - английский словарь
+            Dictionary<string, string> D_eng_deuch = new Dictionary<string, string>(); //англо-немецкий словарь
+            
+
+
+        }
+        bool Dict_Load() // функция загрузки словаря
+        {
+            return false;
+        }
+        bool Dict_Save(Dictionary<string, string> D) // функция сохранения словаря
+        {
+            using (FileStream fs = new FileStream("D.json", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                JsonSerializer.Serialize(fs, D);
+            }
+            return false;
+        }
+        bool Export(string s) // экспорт слова с переводом
+        {
+            return false;
 
         }
     }
